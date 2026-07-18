2ce6299e73ddaa7cb87363e83bea0acb2e7802b5
SBBekele <segni.b2009@gmail.com>
2026-07-18 21:29:44 +0200

    chore(api): migrate API to ESM

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -3,21 +3,23 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+export default defineConfig(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'dist/**'],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+    ],
     languageOptions: {
       globals: {
-        ...globals.node,
-        ...globals.jest,
+        ...globals.nodeBuiltin,
       },
-      sourceType: 'commonjs',
+      sourceType: 'module',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -29,7 +31,21 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+    },
+  },
+  {
+    files: ['src/**/*.spec.ts', 'test/**/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
